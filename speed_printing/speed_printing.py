3a6ce83622c183e7e7c0d01ba4381317bd0e0966
Slavik <yaroslav.salko71@gmail.com>
2024-06-10 13:42:41 +0200

    Cleaned up the code, added text, wrote down the logic for returning to the user menu

--- speed_printing/speed_printing.py
+++ speed_printing/speed_printing.py
@@ -2,19 +2,12 @@ import time
 from levenshtein import levenshtein
 from data_base import *
 
-current_user = None
 
-def main_menu():
-    print('1: Register')
-    print('2: Login')
-    print('3: Exit')
-
+#_____________________________ESPACE REGISTRATION______________________________
 
 def registration_user():
     user_name_input = str(input('Enter your name: '))
     user_password_input = str(input("Enter your password: "))
-    user_len = len(user_name_input)
-    pass_len = len(user_password_input)
     while True:
         if 1 <= len(user_name_input) <= 15 and len(user_password_input) >= 8:
             print('Registration successful')
@@ -25,6 +18,9 @@ def registration_user():
             user_name_input = str(input('Enter your name: '))
             user_password_input = str(input("Enter your password: "))
 
+#_____________________________ESPACE USER_______________________________________
+
+current_user = None
 
 def login():
     global current_user
@@ -33,9 +29,11 @@ def login():
     if user_name_input and user_password_input:   
         while True:
             if not user_login(user_name_input, user_password_input):
-                print('Wrong user name or password')
+                print('Wrong user name or password. For registration press "ENTER"')
                 user_name_input = str(input('Please enter your user name: '))
                 user_password_input = str(input("Enter your password: "))
+                if user_name_input == "" or user_password_input == "":
+                    registration_user()
             else:
                 print("login successful")
                 break
@@ -43,15 +41,65 @@ def login():
         current_user = user_name_input
         return current_user
 
+
 def show_records():
     global current_user
     records = get_all_records(current_user)
+    if not records:
+        user_menu()
     for record in records:
         print(record)
+        user_menu()
+
+#_______________________________ESPACE MENU_____________________________________
+
+def main_menu():
+    print('1: Register')
+    print('2: Login')
+    print('3: Exit')
+
+
+def user_menu():
+    print('1: Start game')
+    print("2: Show records")
+    print("3: Exit to main menu")
+    choice = input('Make your choice: ')
+    if choice == '1':
+        start_game()
+    elif choice == "2":
+        show_records()
+    elif choice == "3":
+        main()
+    else:
+        print('Invalid choice')
+
+
+def main():
+
+    if not table_exist('game_data'):
+        creat_table()
+
+    while True:
+        main_menu()
+        choice = input("Make your choise: ")
+        if choice == '1':
+            registration_user()
+        elif choice == '2':
+            login()
+            if current_user is not None:
+                user_menu()
+        elif choice == "3":
+            break
+        else:
+            print('invalide choice. try again')
+
+#_______________________________ESPACE GAME_____________________________
 
 def start_game():
     global current_user
-    texts = ["Hello","In a small village by the sea, a young girl named Lily discovered a hidden cave filled with ancient treasures. She found gold coins, sparkling jewels, and mysterious maps. Excited by her discovery, Lily shared the news with her friends, and together they embarked on thrilling adventures that would change their lives forever.","Technology has revolutionized how we communicate, allowing people to connect instantly across the globe. Social media platforms enable us to share our lives, ideas, and experiences in real-time. However, it's crucial to balance screen time with face-to-face interactions to maintain strong personal relationships and mental well-being."]
+    texts = ["""The quick brown fox jumps over the lazy dog. This sentence is a classic example of a pangram, a sentence that uses every letter of the alphabet at least once. Typing pangrams is a great way to practice typing because they ensure you cover a wide range of letters and keystrokes.""",
+             """In a small village by the sea, a young girl named Lily discovered a hidden cave filled with ancient treasures. She found gold coins, sparkling jewels, and mysterious maps. Excited by her discovery, Lily shared the news with her friends, and together they embarked on thrilling adventures that would change their lives forever.""",
+             """Technology has revolutionized how we communicate, allowing people to connect instantly across the globe. Social media platforms enable us to share our lives, ideas, and experiences in real-time. However, it's crucial to balance screen time with face-to-face interactions to maintain strong personal relationships and mental well-being."""]
     print('Welcome to the typing speed training game.')
     time.sleep(2)
     for ind, text in enumerate(texts):
@@ -75,32 +123,9 @@ def start_game():
     set_user_achievement(current_user, time_game, accuracy)
     if time_game == get_time_records(current_user):
         print(f'You set a new record: {get_time_records(current_user)}')
+    user_menu()
 
-
-def main():
-
-    if not table_exist('game_data'):
-        creat_table()
-
-    while True:
-        main_menu()
-        choice = input("Make your choise: ")
-        if choice == '1':
-            registration_user()
-        elif choice == '2':
-            if login():
-                print('1: Start game')
-                print("2: Show records")
-                choice = input('Make your choice: ')
-                if choice == '1':
-                    start_game()
-                elif choice == "2":
-                    show_records()
-        elif choice == "3":
-            break
-        else:
-            print('invalide choice. try again')
-
+#__________________________START APPLICATION_______________
 if __name__ == '__main__':
     main()
 
